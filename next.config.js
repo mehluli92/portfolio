3e4fb02ef4e50d70bd097cea8ef34fa1f2279ea2
Mehluli Nokwara <61227518+mehluli92@users.noreply.github.com>
2024-07-19 17:05:11 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   basePath: '/portfolio',
   assetPrefix: '/portfolio/',
-  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
